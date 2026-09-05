c577ab46e7f82552551c1a9ec8dee5d992e4ab2b
Danii <daniellaadegun@gmail.com>
2026-09-05 14:19:04 -0600

    Create main.py
    this is my own story created off the instructions by Mr. Forsyth.

---
+++ main.py
@@ -0,0 +1,11 @@
+noun = input("Noun\n")
+adjective = input("Adjective\n")
+verb = input("Verb ending in -ing\n")
+name = input("Student name\n")
+number = input("Number\n")
+plural_noun = input("Plural noun\n")
+adjective_2 = input("Another adjective\n")
+
+print("The noun you chose was:")
+message= f"My name is {name} and I am {number} years old. I like {noun} and the {noun} she likes is very {adjective}. Another thing {name} likes is {verb}. She {plural_noun} a lot with her friends. {name} is a very {adjective_2} if you get to know her  "
+print(noun)
